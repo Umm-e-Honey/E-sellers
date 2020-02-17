87f52e749445ad8dd662b40ef769271346d4c21b
Umm-e-Honey <honeyummee123@gmail.com>
2020-02-17 17:46:19 +0600

    E-sellers

---
+++ src/main/java/com/example/e_sellers/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.example.e_sellers;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Initialize And Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+
+        // Perform ItemSelectedListener
+
+    }
+}
